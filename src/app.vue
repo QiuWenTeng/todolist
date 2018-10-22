8c8846b638f88441abe7007a8cbb48de6b0631c6
邱雯腾 <qiuwenteng@qiuwentengdeMacBook-Pro.local>
2018-10-23 07:52:46 +0800

    firstcommit

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="test">{{text}}</div>
+</template>
+
+
+<script type="text/javascript">
+export default{
+	data(){
+		return {
+			text: 'abc'
+		}
+	}
+}
+</script>
+
+
+<style type="text/css">
+	
+</style>
