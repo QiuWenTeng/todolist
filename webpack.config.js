1b03ee8e4354932469c6612deec4ba38faf9cb28
邱雯腾 <qiuwenteng@qiuwentengdeMacBook-Pro.local>
2018-10-30 08:30:46 +0800

    devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const isDev = process.env.NODE_ENV === 'development'
 
-module.exports = {
+const config = {
+	target: 'web',
 	entry: path.join(__dirname, 'src/index.js'),
 	output: {
 		filename: 'bundle.js',
@@ -32,5 +36,35 @@ module.exports = {
 				]
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env':{
+				NODE_ENV: isDev ? '"development"' : '"production"'
+			}
+		}),
+		new HTMLPlugin()
+	]
 }
+
+if (isDev) {
+	config.devtool = '#cheap-module-eval-source-map'
+	config.devServer = {
+		port: 8000,
+		host: '0.0.0.0',
+		overlay: {
+			errors: true
+		},
+		open: true,
+		hot: true
+		// historyFallback
+	}
+	config.plugins.push(
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin()
+	)
+}
+
+module.exports = config
+
+
