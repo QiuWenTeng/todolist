9596d69a4875829e593622f5be2b1b874ae75f4f
邱雯腾 <qiuwenteng@qiuwentengdeMacBook-Pro.local>
2018-11-06 08:20:13 +0800

    peizhi

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ const config = {
 				loader: 'vue-loader'
 			},
 			{
+				test: /\.jsx$/,
+				loader: 'babel-loader'
+			},
+			{
 				test: /\.css$/,
 				use: [
 					'style-loader',
@@ -34,6 +38,21 @@ const config = {
 						}
 					}
 				]
+			},
+			{
+				test: /\.styl/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap:true,
+						}
+					},
+					'stylus-loader'
+				]
+
 			}
 		]
 	},
